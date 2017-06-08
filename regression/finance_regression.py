a9b2815516bc314c4fd8e900dbcec3b3372a7b5d
Engen Sherpa <enggenapp@gmail.com>
2017-06-08 16:15:21 +0100

    Update finance_regression.py

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -17,19 +17,23 @@ import sys
 import pickle
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
-dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
+
+dictionary = "../final_project/final_project_dataset_modified.pkl"
+
+with open(dictionary, 'rb') as words_file_handler:
+        dictionary = pickle.load(words_file_handler)
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
-data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
+data = featureFormat(dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
 ### training-testing split needed in regression, just like classification
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -37,10 +41,14 @@ test_color = "b"
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
+from sklearn.linear_model import LinearRegression
 
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
 
-
-
+print('Slope:', reg.coef_[0], 'Intercept:', reg.intercept_)
+print('Training Score:', reg.score(feature_train, target_train))
+print('Testing Score:', reg.score(feature_test, target_test))
 
 
 
@@ -64,6 +72,8 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b") 
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
